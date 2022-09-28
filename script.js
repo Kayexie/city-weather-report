c61386dc2c2a2ef51f47c6f7cfc26135dc377f67
Kaye Xie <kayexie@Kayes-MacBook-Air.local>
2022-09-27 22:44:51 -0400

    add search history

--- script.js
+++ script.js
@@ -2,25 +2,57 @@ var citySearchEl = document.querySelector('#citySearch');//form to search city
 var submitBtn =  document.querySelector('#submit')//the search button;
 var cityInput = document.querySelector('#cityinput')// the input area;
 var showWeatherEl = document.querySelector('#showWeather')// the space to show weahter;
+var searchHistoryEl = document.querySelector('#historycity');//show the historical search city;
+console.log(searchHistoryEl)
 
-console.log(submitBtn)
-console.log(cityInput)
+
+var cityName = cityInput.value.trim();
+var cityArr=[];
+
+function renderCity() {
+  searchHistoryEl.innerHTML='';
+  for (var i=0; i<cityArr.length;i++){
+  var city = cityArr[i];
+  var cityLi = document.createElement('button');
+  cityLi.textContent = city;
+  searchHistoryEl.appendChild(cityLi);
+}}
+
+function init() {
+ var getCities = JSON.parse(localStorage.getItem('key'));
+    if (getCities !== null) {
+      cityArr = getCities;
+    renderCity();
+  }}
+
+function storeCity() {
+  localStorage.setItem('key', JSON.stringify(cityArr));
+  }
 
 function handleSubmit(event) {
     event.preventDefault();
     var cityName = cityInput.value.trim();
+    // var cityNameOne = String(cityName)
     if (cityName) {
         getCityRepo(cityName);
+
+    cityArr.push(cityName);
     showWeatherEl.textContent = '';
     } else {
         alert('Please Input City Name!');
     }
+  storeCity();
+  renderCity();
 };
 
+init();
+console.log(cityArr);
+
+var apiKey = '050f6c384571e8bc7593ff44448e1672';
 
 function getCityRepo(cityName) {
 
-    var apiKey = '050f6c384571e8bc7593ff44448e1672';
+   
 
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
 
@@ -30,6 +62,7 @@ function getCityRepo(cityName) {
         response.json().then(function(data){
             console.log(data)
         
+        var icon = document.createElement('i')
         var weahterUl = document.createElement('ul');
         var city = document.createElement('li');
         var temp = document.createElement('li');
@@ -56,9 +89,18 @@ function getCityRepo(cityName) {
     .catch(function (error)  {
         alert('Unable to connect')
     });
+fiveDayForecast();
 }
 
+function fiveDayForecast() {
+  var cityName = cityInput.value.trim();
+  var apiUrlTwo = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
+  fetch(apiUrlTwo)
+  .then(function(response){
+    response.json().then(function(data){
+    console.log(data)
+    
+    });
+})}
 
 citySearchEl.addEventListener('submit', handleSubmit);
-
-console.log(citySearchEl)
