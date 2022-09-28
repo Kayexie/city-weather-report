5898d09285c08ce0189ac5f6c57eecd2b1d98192
Kaye Xie <kayexie@Kayes-MacBook-Air.local>
2022-09-28 00:08:34 -0400

    add forecast

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var citySearchEl = document.querySelector('#citySearch');//form to search city
 var submitBtn =  document.querySelector('#submit')//the search button;
 var cityInput = document.querySelector('#cityinput')// the input area;
-var showWeatherEl = document.querySelector('#showWeather')// the space to show weahter;
+var showCurrentWeatherEl = document.querySelector('#currentweather')// the space to show current weahter;
 var searchHistoryEl = document.querySelector('#historycity');//show the historical search city;
-console.log(searchHistoryEl)
+
+var dayOneEl = document.querySelector('#dayone')
+var dayTwoEl = document.querySelector('#daytwo')
+var dayThreeEl = document.querySelector('#daythree')
+var dayFourEl = document.querySelector('#dayfour')
+var dayFiveEl = document.querySelector('#dayfive');
+
+
+
+console.log(dayOneEl)
 
 
 var cityName = cityInput.value.trim();
@@ -16,7 +25,10 @@ function renderCity() {
   var cityLi = document.createElement('button');
   cityLi.textContent = city;
   searchHistoryEl.appendChild(cityLi);
-}}
+}
+
+getCityRepo();
+}
 
 function init() {
  var getCities = JSON.parse(localStorage.getItem('key'));
@@ -37,7 +49,7 @@ function handleSubmit(event) {
         getCityRepo(cityName);
 
     cityArr.push(cityName);
-    showWeatherEl.textContent = '';
+    showCurrentWeatherEl.textContent = '';
     } else {
         alert('Please Input City Name!');
     }
@@ -62,7 +74,6 @@ function getCityRepo(cityName) {
         response.json().then(function(data){
             console.log(data)
         
-        var icon = document.createElement('i')
         var weahterUl = document.createElement('ul');
         var city = document.createElement('li');
         var temp = document.createElement('li');
@@ -75,7 +86,7 @@ function getCityRepo(cityName) {
         humidity.innerHTML = 'Humidity: ' + data['main'].humidity + '%';
 
 
-        showWeatherEl.appendChild(weahterUl);
+        showCurrentWeatherEl.appendChild(weahterUl);
         weahterUl.appendChild(city);
         weahterUl.appendChild(temp);
         weahterUl.appendChild(wind);
@@ -98,8 +109,118 @@ function fiveDayForecast() {
   fetch(apiUrlTwo)
   .then(function(response){
     response.json().then(function(data){
-    console.log(data)
-    
+    var dateArr = data['list'];
+    dateArr.length = 100;
+    console.log(dateArr);
+//first day of the forecase weather;
+    var dateOne = dateArr[2];
+    console.log(dateOne);
+    var weahterUlOne = document.createElement('ul');
+    var dayOne = document.createElement('li')
+    var tempOne = document.createElement('li');
+    var windOne = document.createElement('li');
+    var humidityOne = document.createElement('li');
+  
+    dayOne.innerHTML = 'Date: ' + dateOne['dt_txt'];
+    tempOne.innerHTML = 'Temperature: ' + dateOne['main'].temp + ' °F';
+    windOne.innerHTML = 'Wind: ' + dateOne['wind'].speed + ' MPH';
+    humidityOne.innerHTML = 'Humidity: ' + dateOne['main'].humidity + '%';
+
+    console.log(dayOne.innerHTML);
+    console.log(tempOne.innerHTML);
+    console.log(windOne.innerHTML);
+    console.log(humidityOne.innerHTML);
+  
+    dayOneEl.appendChild(weahterUlOne);
+    weahterUlOne.appendChild(dayOne);
+    weahterUlOne.appendChild(tempOne);
+    weahterUlOne.appendChild(windOne);
+    weahterUlOne.appendChild(humidityOne); 
+  
+//second day of the forecast weather;
+
+    var dateTwo = dateArr[10];
+    console.log(dateTwo);
+    var weahterUlTwo = document.createElement('ul');
+    var dayTwo = document.createElement('li')
+    var tempTwo = document.createElement('li');
+    var windTwo = document.createElement('li');
+    var humidityTwo = document.createElement('li');
+  
+    dayTwo.innerHTML = 'Date: ' + dateTwo['dt_txt'];
+    tempTwo.innerHTML = 'Temperature: ' + dateTwo['main'].temp + ' °F';
+    windTwo.innerHTML = 'Wind: ' + dateTwo['wind'].speed + ' MPH';
+    humidityTwo.innerHTML = 'Humidity: ' + dateTwo['main'].humidity + '%';
+  
+    dayTwoEl.appendChild(weahterUlTwo);
+    weahterUlTwo.appendChild(dayTwo);
+    weahterUlTwo.appendChild(tempTwo);
+    weahterUlTwo.appendChild(windTwo);
+    weahterUlTwo.appendChild(humidityTwo);
+
+//Third day of the forecast weather;
+
+var dateThree = dateArr[18];
+console.log(dateThree);
+var weahterUlThree = document.createElement('ul');
+var dayThree = document.createElement('li')
+var tempThree = document.createElement('li');
+var windThree = document.createElement('li');
+var humidityThree = document.createElement('li');
+
+dayThree.innerHTML = 'Date: ' + dateThree['dt_txt'];
+tempThree.innerHTML = 'Temperature: ' + dateThree['main'].temp + ' °F';
+windThree.innerHTML = 'Wind: ' + dateThree['wind'].speed + ' MPH';
+humidityThree.innerHTML = 'Humidity: ' + dateThree['main'].humidity + '%';
+
+dayThreeEl.appendChild(weahterUlThree);
+weahterUlThree.appendChild(dayThree);
+weahterUlThree.appendChild(tempThree);
+weahterUlThree.appendChild(windThree);
+weahterUlThree.appendChild(humidityThree);
+
+//Forth day of the forecast weather;
+
+var dateFour = dateArr[26];
+console.log(dateFour);
+var weahterUlFour = document.createElement('ul');
+var dayFour = document.createElement('li')
+var tempFour = document.createElement('li');
+var windFour = document.createElement('li');
+var humidityFour = document.createElement('li');
+
+dayFour.innerHTML = 'Date: ' + dateFour['dt_txt'];
+tempFour.innerHTML = 'Temperature: ' + dateFour['main'].temp + ' °F';
+windFour.innerHTML = 'Wind: ' + dateFour['wind'].speed + ' MPH';
+humidityFour.innerHTML = 'Humidity: ' + dateFour['main'].humidity + '%';
+
+dayFourEl.appendChild(weahterUlFour);
+weahterUlFour.appendChild(dayFour);
+weahterUlFour.appendChild(tempFour);
+weahterUlFour.appendChild(windFour);
+weahterUlFour.appendChild(humidityFour);
+
+//Fifth day of the forecast weather;
+
+var dateFive = dateArr[34];
+console.log(dateFive);
+var weahterUlFive = document.createElement('ul');
+var dayFive = document.createElement('li')
+var tempFive = document.createElement('li');
+var windFive = document.createElement('li');
+var humidityFive = document.createElement('li');
+
+dayFive.innerHTML = 'Date: ' + dateFive['dt_txt'];
+tempFive.innerHTML = 'Temperature: ' + dateFive['main'].temp + ' °F';
+windFive.innerHTML = 'Wind: ' + dateFive['wind'].speed + ' MPH';
+humidityFive.innerHTML = 'Humidity: ' + dateFive['main'].humidity + '%';
+
+dayFiveEl.appendChild(weahterUlFive);
+weahterUlFive.appendChild(dayFive);
+weahterUlFive.appendChild(tempFive);
+weahterUlFive.appendChild(windFive);
+weahterUlFive.appendChild(humidityFive);
+
     });
 })}
 
